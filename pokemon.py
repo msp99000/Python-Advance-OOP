c20ec41b0fabf4378f59b53e14519ce5921e78f4
msp99000 <msp99000@gmail.com>
2022-03-16 09:50:58 +0530

    Added pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,10 @@
+'''
+
+Key Objectives
+    - Create a Pokemon
+    - View its details
+    - Feed them to increase their health
+    - Make them battle against each other and choose a winner
+
+'''
+
